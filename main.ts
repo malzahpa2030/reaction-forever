191577eda3c9c74841472907ea055b9e376f0fb3
malzahpa2030 <83504564+malzahpa2030@users.noreply.github.com>
2021-05-03 15:59:31 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+let score1 = 0
+let score2 = 0
+input.onPinPressed(TouchPin.P0, function () {
+    score1 = 0
+    score2 = 0
+    basic.pause(10000)
+    if (score1 > score2) {
+        basic.showString("Player 1 wins")
+    } else if (score1 < score2) {
+        basic.showString("Player 2 wins")
+    }
+    basic.showString("player1:" + score1)
+    basic.showString("player2:" + score2)
+})
+input.onPinPressed(TouchPin.P2, function () {
+    score2 += 1
+})
+input.onPinPressed(TouchPin.P1, function () {
+    score1 += 1
+})
